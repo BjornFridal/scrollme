a0743f6472c443092d4f6820418b367281dd6a7c
LAPTOP-3BV7JSUB\Gabriel <gpeto1991@gmail.com>
2016-11-04 16:25:04 -0200

    Initialization no longer occurs automatically. You need to declare a variable in your js, passing the scrollme object as value and call the init () method manually. The init function now receives a disable_on_mobile parameter to prevent the animations when mobile. Also added function to check whether it is a mobile device or not.

--- jquery.scrollme.js
+++ jquery.scrollme.js
@@ -93,18 +93,36 @@ var scrollme = ( function( $ )
 	];
 
 	// ----------------------------------------------------------------------------------------------------
+	// Check if is mobile
+	_this.is_mobile = function() {
+		var check = false;
+  		
+  		(function( a ) {
+  			if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) {
+  				check = true;
+  			}
+  		})(navigator.userAgent||navigator.vendor||window.opera);
+  		
+  		return check;
+	}
+
+	// ----------------------------------------------------------------------------------------------------
 	// Initialisation conditions
 
-	_this.init_if = function() { return true; }
+	_this.init_if = function() { return true }
 
 	// ----------------------------------------------------------------------------------------------------
 	// Initialisation
 
-	_this.init = function()
+	_this.init = function(disable_on_mobile)
 	{
-		// Cancel if initialisation conditions not met
-
-		if( !_this.init_if() ) return false;
+		// Cancel initialization if the mobile option is set as a parameter
+		
+		if(disable_on_mobile && disable_on_mobile === "disable_on_mobile") {
+			if(_this.is_mobile()) return false;
+		} else if(disable_on_mobile) {
+			throw "unknown parameter '" + disable_on_mobile + "' on init()";
+		}
 
 		// Load all elements to animate
 
@@ -180,17 +198,17 @@ var scrollme = ( function( $ )
 
 				var properties = {};
 
-				if( effect.element.is( '[data-opacity]' ) )    properties.opacity    = effect.element.data( 'opacity' );
-				if( effect.element.is( '[data-translatex]' ) ) properties.translatex = effect.element.data( 'translatex' );
-				if( effect.element.is( '[data-translatey]' ) ) properties.translatey = effect.element.data( 'translatey' );
-				if( effect.element.is( '[data-translatez]' ) ) properties.translatez = effect.element.data( 'translatez' );
-				if( effect.element.is( '[data-rotatex]' ) )    properties.rotatex    = effect.element.data( 'rotatex' );
-				if( effect.element.is( '[data-rotatey]' ) )    properties.rotatey    = effect.element.data( 'rotatey' );
-				if( effect.element.is( '[data-rotatez]' ) )    properties.rotatez    = effect.element.data( 'rotatez' );
-				if( effect.element.is( '[data-scale]' ) )      properties.scale      = effect.element.data( 'scale' );
-				if( effect.element.is( '[data-scalex]' ) )     properties.scalex     = effect.element.data( 'scalex' );
-				if( effect.element.is( '[data-scaley]' ) )     properties.scaley     = effect.element.data( 'scaley' );
-				if( effect.element.is( '[data-scalez]' ) )     properties.scalez     = effect.element.data( 'scalez' );
+				if( effect.element.is( '[data-opacity]' ) )    	   properties.opacity       = effect.element.data( 'opacity' );
+				if( effect.element.is( '[data-translatex]' ) ) 	   properties.translatex    = effect.element.data( 'translatex' );
+				if( effect.element.is( '[data-translatey]' ) )	   properties.translatey    = effect.element.data( 'translatey' );
+				if( effect.element.is( '[data-translatez]' ) ) 	   properties.translatez    = effect.element.data( 'translatez' );
+				if( effect.element.is( '[data-rotatex]' ) )    	   properties.rotatex       = effect.element.data( 'rotatex' );
+				if( effect.element.is( '[data-rotatey]' ) )    	   properties.rotatey       = effect.element.data( 'rotatey' );
+				if( effect.element.is( '[data-rotatez]' ) )    	   properties.rotatez       = effect.element.data( 'rotatez' );
+				if( effect.element.is( '[data-scale]' ) )      	   properties.scale         = effect.element.data( 'scale' );
+				if( effect.element.is( '[data-scalex]' ) )     	   properties.scalex        = effect.element.data( 'scalex' );
+				if( effect.element.is( '[data-scaley]' ) )     	   properties.scaley        = effect.element.data( 'scaley' );
+				if( effect.element.is( '[data-scalez]' ) )     	   properties.scalez        = effect.element.data( 'scalez' );
 
 				effect.properties = properties;
 
@@ -200,6 +218,7 @@ var scrollme = ( function( $ )
 			element.effects = effects;
 
 			_this.elements.push( element );
+			//console.dir(element.effects);
 		});
 	}
 
@@ -242,6 +261,12 @@ var scrollme = ( function( $ )
 			for( var e=0 ; e<effects_length ; e++ )
 			{
 				var effect = element.effects[e];
+
+				console.dir(effect.properties);
+
+				if( effect.properties.disableMobile && effect.properties.disableMobile === "true" ) {
+					console.log('nao anima viadu');
+				}
 
 				// Get effect animation boundaries
 
@@ -444,7 +469,7 @@ var scrollme = ( function( $ )
 	// ----------------------------------------------------------------------------------------------------
 	// Bind initialisation
 
-	$document.on( _this.init_events.join( ' ' ) , function(){ _this.init(); } );
+	//$document.on( _this.init_events.join( ' ' ) , function(){ _this.init(); } );
 
 	// ----------------------------------------------------------------------------------------------------
 
